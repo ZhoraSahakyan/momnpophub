d8bcd491741bcc0928833607c3651d0f848028ff
Zhora Sahakyan <joko.sahakyan95@gmail.com>
2020-09-16 22:00:21 +0400

    payment settings page

--- src/components/block-header/Block-header.js
+++ src/components/block-header/Block-header.js
@@ -11,12 +11,12 @@ const BlockHeader = ({title, buttonText, buttonIcon}) => {
         <header
             className="d-flex justify-content-between justify-content-center secttion-container__header">
             <h2 className="title m-0">{title}</h2>
-            <div className="d-flex">
+            {buttonIcon && buttonText && <div className="d-flex">
                 <Button type="button" className="btn border-0 bg-transparent shadow-none p-0">
                     <i className={icon}/>
                     <span className="orange-btn">{buttonText}</span>
                 </Button>
-            </div>
+            </div>}
         </header>
     )
 };
--- src/root/App.js
+++ src/root/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import {BrowserRouter as Router, Redirect, Route, Switch,} from "react-router-dom";
+import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
 
 import './App.css';
+
+import PaymentSettings from "../pages/paymant-settings/Payment-settings";
 import BusinessHome from "../pages/bussines-home/Business-home";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
@@ -14,7 +16,8 @@ function App() {
             <Router>
                 <Switch>
                     <Route path="/business-home" component={BusinessHome}/>
-                    <Redirect to="/business-home"/>
+                    <Route exac path="/payment-settings" component={PaymentSettings}/>
+                    <Redirect to="/payment-settings"/>
                 </Switch>
             </Router>
 
